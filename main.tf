674715355ab0a5b090b367e5dbd3c9e579d0d5b6
sachin singh <sachinsinsinwar8@gmail.com>
2025-11-27 09:13:59 +0000

    Add ALB with security group and second public subnet

--- main.tf
+++ main.tf
@@ -47,6 +47,19 @@ resource "aws_subnet" "public" {
   }
 }
 
+resource "aws_subnet" "public_b" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.3.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "${var.aws_region}b"
+
+  tags = {
+    Name    = "cloudsec-public-subnet-b"
+    Project = "aws-secure-vpc-terraform"
+    Tier    = "public"
+  }
+}
+
 # -----------------------------
 # Private Subnet
 # -----------------------------
@@ -105,7 +118,7 @@ resource "aws_route_table_association" "public_assoc" {
 
 # Elastic IP for NAT Gateway
 resource "aws_eip" "nat_eip" {
-  vpc = true
+  domain = "vpc"
 
   tags = {
     Name    = "cloudsec-nat-eip"
@@ -147,4 +160,72 @@ resource "aws_route_table" "private_rt" {
 resource "aws_route_table_association" "private_assoc" {
   subnet_id      = aws_subnet.private.id
   route_table_id = aws_route_table.private_rt.id
+}
+
+# -----------------------------
+# Security Group for ALB
+# -----------------------------
+
+resource "aws_security_group" "alb_sg" {
+  name        = "cloudsec-alb-sg"
+  description = "Allow HTTP from anywhere"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description = "HTTP from anywhere"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description = "Allow all outbound"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name    = "cloudsec-alb-sg"
+    Project = "aws-secure-vpc-terraform"
+  }
+}
+
+# -----------------------------
+# Application Load Balancer (ALB)
+# -----------------------------
+
+resource "aws_lb" "alb" {
+  name               = "cloudsec-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.alb_sg.id]
+  subnets = [
+    aws_subnet.public.id,
+    aws_subnet.public_b.id
+  ]
+
+  tags = {
+    Name    = "cloudsec-alb"
+    Project = "aws-secure-vpc-terraform"
+  }
+}
+
+# Listener: simple HTTP fixed response
+resource "aws_lb_listener" "http_listener" {
+  load_balancer_arn = aws_lb.alb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "CloudSec ALB is working"
+      status_code  = "200"
+    }
+  }
 }
